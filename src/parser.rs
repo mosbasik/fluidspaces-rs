0fb91254c4d24bae0421158769a8d75c2cd004f2
Peter Henry <me@peterhenry.net>
2018-11-15 13:54:03 -0800

    cut commented old parser code + cleanup

--- src/parser.rs
+++ src/parser.rs
@@ -8,18 +8,6 @@ use nom::{digit, rest};
 
 use std::str::from_utf8;
 
-// use std;
-
-// // defines "usize_parser"
-// // returns a usize parsed from the input string?
-// named!(
-//     usize_parser<usize>,
-//     map_res!(
-//         map_res!(digit, std::str::from_utf8),
-//         std::str::FromStr::from_str
-//     )
-// );
-
 // parser matches as many digits as possible; errors if no digits present
 named!(number<Input, Input>, call!(digit));
 
@@ -29,7 +17,8 @@ named!(colon<Input, Input>, tag!(":"));
 // parser matches all input until the end; outputs everything matched
 named!(the_rest<Input, Input>, call!(rest));
 
-named!(pub title_parser<Input, (Option<Input>, Option<Input>)>,
+// parser returns tuple of (number, name), both are optional
+named!(title_parser<Input, (Option<Input>, Option<Input>)>,
     do_parse!(
         num: opt!(ws!(number)) >>
         opt!(ws!(colon)) >>
@@ -41,24 +30,9 @@ named!(pub title_parser<Input, (Option<Input>, Option<Input>)>,
     )
 );
 
-// // defines "title_parser"
-// // returns the rest of the input
-// named!(title_parser<&str>, map_res!(rest, std::str::from_utf8));
-
-// // defines "parse_title_from_name"
-// // in order:
-// //   parses and discards a usize (optional)
-// //   parses and discards a colon (optional)
-// //   returns the rest of the input
-// named!(pub parse_title_from_name<&str>, dbg!(
-//     do_parse!(
-//         opt!(ws!(usize_parser)) >>
-//         opt!(ws!(tag!(":"))) >>
-//         title: ws!(title_parser) >>
-//         (title)
-//     )
-// ));
-
+// public interface of the parser - give it the i3-formatted name of the
+// workspace and it tries to give you back a useful title (without numbers, if
+// possible)
 pub fn title_from_name<'a>(name: &'a str) -> Result<&'a str, Error> {
     match title_parser(Input(name)) {
         Ok((_, (number, name))) => Ok(from_utf8(name.or(number).unwrap().as_bytes())?),
@@ -84,9 +58,7 @@ mod tests {
         fn $name() {
             let (input, expected_out): (&str, nom::IResult<Input, Input>) = $value;
             match $parser(Input(input)) {
-                Ok(actual_out) => assert_eq!(
-                    actual_out,
-                    expected_out.expect("Parser gave an Ok() but test expects an Err()")),
+                Ok(actual_out) => assert_eq!(actual_out, expected_out.expect("Parser gave an Ok() but test expects an Err()")),
                 Err(actual_out) => assert_eq!(
                     actual_out,
                     expected_out.expect_err("Parser gave an Err() but test expects an Ok()"))
@@ -183,42 +155,4 @@ mod tests {
             number_title_with_missing_index_missing_colon: ("2", (Some(Input("2")), None)),
         }
     }
-
-    // // define a macro that can generate test functions for us
-    // macro_rules! title_from_name_tests {
-    //     ($($name:ident: $value:expr,)*) => {
-    //         $(
-    //             #[test]
-    //             fn $name() {
-    //                 let (input, expected) = $value;
-    //                 assert_eq!(title_from_name(input).unwrap(), expected);
-    //             }
-    //         )*
-    //     };
-    // }
-
-    // title_from_name_tests! {
-    //     basic: ("7:foo", "foo"),
-
-    //     number_is_multidigit: ("77:foo", "foo"),
-    //     number_is_missing: (":foo", "foo"),
-    //     number_and_colon_are_missing: ("foo", "foo"),
-
-    //     space_after_colon: ("7: foo", "foo"),
-    //     space_before_colon: ("7 :foo", "foo"),
-    //     space_around_colon: ("7 : foo", "foo"),
-
-    //     title_has_space: ("7:foo bar", "foo bar"),
-    //     title_has_hyphen: ("7:foo-bar", "foo-bar"),
-    //     title_has_apostrophe: ("7:foo'bar", "foo'bar"),
-
-    //     title_has_number_inside: ("7:fo2o", "fo2o"),
-    //     title_has_number_after: ("7:foo2", "foo2"),
-    //     title_has_number_before: ("7:2foo", "2foo"),
-    //     title_has_numberspace_before: ("7:2 foo", "2 foo"),
-
-    //     number_title_with_present_index_present_colon: ("7:2", "2"),
-    //     number_title_with_missing_index_present_colon: (":2", "2"),
-    //     // number_title_with_missing_index_missing_colon: ("2", "2"),
-    // }
 }
